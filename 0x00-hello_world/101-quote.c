0eeeb2ee070cc9c01dee527ad2f50a6797e76d40
tobikiller <edwardnguimeya@yahoo.com>
2022-09-08 13:46:22 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -7,5 +8,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);	
+	return (1);
 }
