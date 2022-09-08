b2235a779d712b361c3a1b6160abac2bb82d06b9
tobikiller <edwardnguimeya@yahoo.com>
2022-09-08 09:59:36 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
+ *  main -entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *  Return: Always 0 (success)
+ */
 
 int main(void)
 {
