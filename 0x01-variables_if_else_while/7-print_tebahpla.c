d743ec077a4163d509df644ef50b69fedc0d63b7
tobikiller <edwardnguimeya@yahoo.com>
2022-09-09 15:34:49 -0700

    reverse letter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int Ch;
+
+	for (Ch = 'z'; Ch >= 'a'; Ch--)
+	{
+	putchar(Ch);
+
+	}
+
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
