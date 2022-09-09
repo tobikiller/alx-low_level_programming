b94b7d53e9e4218324de953fbb106a598df10bd8
tobikiller <edwardnguimeya@yahoo.com>
2022-09-09 14:15:50 -0700

    Exclude e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int Ch;
+
+	for (Ch = 'a'; Ch <= 'z'; Ch++)
+	{
+	if(Ch != 'e' && Ch != 'q')
+	{
+
+	putchar(Ch);
+
+	}
+
+	}
+
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
