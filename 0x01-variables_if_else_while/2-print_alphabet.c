82a27b3e376dc881a39ab7cf4c2997d76fafa980
tobikiller <edwardnguimeya@yahoo.com>
2022-09-09 11:02:39 -0700

    Alaphabet order

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int Ch;
+
+	for (Ch = 'a'; Ch <= 'z'; Ch++)
+	{
+
+	putchar(Ch);
+
+	}
+
+	return (0);
+}
