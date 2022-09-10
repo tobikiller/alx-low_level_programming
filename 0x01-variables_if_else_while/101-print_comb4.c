085b4e45cac810c8d0c64b375b1ee01db124d195
tobikiller <edwardnguimeya@yahoo.com>
2022-09-10 15:43:28 -0700

    triple number

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,6 +10,28 @@ int main(void)
 {
 	int num0, num1, num2;
 
-	for(num0 = 0)
+    for (num0 = '0'; num0 <= '7'; num0++ )
+    {
+        for (num1 = '1'; num1 <= '8'; num1++)
+        {
+        for (num2 = '2'; num2 <= '9'; num2++)
+        {
+	if (num0 != num1 && num0 != num2 && num0 < num1 && num1 < num2)
+	{
+        putchar(num0);
+        putchar(num1);
+        putchar(num2);
+
+        if (!(num0 == '7' && num1 == '8' && num2 == '9'))
+	{
+	putchar(',');
+	putchar(' ');
+        }
+	}
+        }
+	}
+
+    }
+	putchar('\n');
 	return (0);
 }
