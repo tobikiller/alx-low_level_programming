a00152d94fb9f4e0fe305e04e5d14bb2e29a7690
tobikiller <edwardnguimeya@yahoo.com>
2022-09-09 18:43:46 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,25 @@
 
 int main(void)
 {
+	int n = 1;
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
 
-	for(num2 = 0; num2 < 10; num2++)
+	for (num2 = n; num2 <= 9 ; num2++)
 	{
-	putchar((num1 % 10) + '0');
-	putchar((num2 % 10) + '0');
+	putchar(num1  + '0');
+	putchar(num2  + '0');
+
 	if (num1 == 9 && num2 == 9)
 	continue;
+
 	putchar(',');
 	putchar(' ');
-	}
 
+	}
+	n++;
 
 	}
 
