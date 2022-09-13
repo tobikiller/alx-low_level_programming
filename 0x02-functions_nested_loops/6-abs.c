4cdae2ed1f9a1ca6468b21f819336ca475e69732
tobikiller <edwardnguimeya@yahoo.com>
2022-09-13 15:12:16 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value.
+ * @n: absolute value
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+
+
+}
