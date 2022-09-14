d46bab3019e13ba4fb2011e38bba3094f024a3f0
tobikiller <edwardnguimeya@yahoo.com>
2022-09-13 18:56:50 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints last digit.
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+
+{
+
+	int min;
+	int hr;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+		_putchar((hr / 10) + '0');
+		_putchar((hr % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min / 10) + '0');
+
+		_putchar('\n');
+		}
+	}
+
+
+}
