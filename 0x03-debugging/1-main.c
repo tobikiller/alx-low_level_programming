657bfa277054131ee11885ccaa2a3fce93911563
tobikiller <edwardnguimeya@yahoo.com>
2022-09-14 10:53:12 -0700

    adding comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - causes an infinite loop
-* don’t delete it!
 * Return: 0
 */
 
@@ -13,12 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-	putchar(i);
-	i++;
-	}
+/*
+*	while (i < 10)
+*	{
+*	putchar(i);
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
