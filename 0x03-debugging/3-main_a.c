2a2f0413398e371c18b54757ad285094501f3c8f
tobikiller <edwardnguimeya@yahoo.com>
2022-09-14 13:28:47 -0700

    date

---
+++ 0x03-debugging/3-main_a.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+
+
+	return (0);
+
+}
