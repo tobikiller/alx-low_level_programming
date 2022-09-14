be870abf5772a8f77bea4082bae4cf0c65e495b9
tobikiller <edwardnguimeya@yahoo.com>
2022-09-14 06:00:24 -0700

    Multiple mains debuged

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - degugging
+ * @i: interger
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+
+
+		if (i > 0)
+		{
+		printf("%d is positive\n", i);
+		} else if (i == 0)
+		{
+		printf("%d is zero\n", i);
+		} else
+
+		printf("%d is negative\n", i);
+
+
+}
