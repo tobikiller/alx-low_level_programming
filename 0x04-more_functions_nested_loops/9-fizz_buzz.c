193ea99f15fc41a9b9a014f2ad008c6bb4888c32
tobikiller <edwardnguimeya@yahoo.com>
2022-09-15 15:41:26 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	int i;
-	for(i = 1; i <= 100; i++)
+
+	for (i = 1; i <= 100; i++)
 	{
+
 	if (i % 15 == 0)
 	printf("FizzBuzz ");
 	else if ((i % 3) == 0)
@@ -19,6 +21,7 @@ int main(void)
 	printf("Buzz ");
 	else
 	printf("%d ", i);
+
 	}
 
 	return (0);
