75959fc666503533fa6720586ecc02c44b9ee14a
tobikiller <edwardnguimeya@yahoo.com>
2022-09-15 11:35:19 -0700

    printed lines;

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: number of line
+ * Return: Always 0.
+ */
+
+
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+	while (a <= n)
+	{
+	_putchar('_');
+	a++;
+	}
+	_putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
