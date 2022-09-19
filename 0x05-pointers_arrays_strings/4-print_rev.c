21dd8cf880789d9998eaac66de67bbd6618aada7
tobikiller <edwardnguimeya@yahoo.com>
2022-09-19 16:49:59 -0700

    reverse letter

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int a;
 
-	while (s[index++])
-	len++;
-	for (index = len - 1; index >= 0; index--)
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	for (a = a - 1; a >= 0; a--)
 
-	_putchar(s[index]);
+	_putchar(s[a]);
 	_putchar('\n');
-
 }
