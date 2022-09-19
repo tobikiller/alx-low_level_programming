550342aabc219733ab453774dedbd01c6ba4d7d0
tobikiller <edwardnguimeya@yahoo.com>
2022-09-19 15:06:14 -0700

    puts prints

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints string
+ * @str: character
+ * Return: return string.
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while(str[c] != '\0')
+	{
+	_putchar(str[c]);
+	c++;
+	}
+
+	_putchar('\n');
+}
