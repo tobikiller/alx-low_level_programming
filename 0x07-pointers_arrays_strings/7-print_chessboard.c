1446aade81fb278e1a43eb13ed108f3a557e8d4e
tobikiller <edwardnguimeya@yahoo.com>
2022-09-26 11:06:37 -0700

    print chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - check the code
+ * @a: string
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+	{
+	if (j == 7)
+	{
+	_putchar(a[i][j]);
+	_putchar('\n');
+	}
+	else
+	_putchar(a[i][j]);
+	}
+	}
+
+
+}
