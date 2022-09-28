947271c790fa1d429310e08993afd3cfb1e74410
tobikiller <edwardnguimeya@yahoo.com>
2022-09-28 16:05:54 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: string.
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
