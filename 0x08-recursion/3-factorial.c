bd42f78ecab35b2d9b1da9b8755dd587195dceef
tobikiller <edwardnguimeya@yahoo.com>
2022-09-28 17:52:03 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * factorial - check the code
+ * @n: number
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+
+	return (-1);
+
+	else if (n == 0)
+	return (0);
+
+	return (n * factorial(n - 1));
+}
