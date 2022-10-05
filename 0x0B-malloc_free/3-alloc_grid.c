dab8c45d6ffae72b8f943863eab58f73156e0a6a
tobikiller <edwardnguimeya@yahoo.com>
2022-10-04 19:27:47 -0700

    rows and colums

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,32 +11,36 @@
  * Otherwise - a pointer to the 2-dimensional array of integers.
  */
 
-
 int **alloc_grid(int width, int height)
 {
-	int **rw;
-	int i, j;
+	int **twoD;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	twoD = malloc(sizeof(int *) * height);
+	if (twoD == NULL)
+
 	return (NULL);
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+	twoD[hgt_index] = malloc(sizeof(int) * width);
 
+	if (twoD[hgt_index] == NULL)
+	{
 
-	rw = malloc(sizeof(int *) * width);
-
-	if (rw == NULL)
+	for (; hgt_index >= 0; hgt_index--)
+	free(twoD[hgt_index]);
+	free(twoD);
 	return (NULL);
-
-	for (i = 0; i < width; i++)
-	{
-	rw[i] = malloc(sizeof(int) * height);
 	}
 
-	for (i = 0; i < width; i++)
-	{
-	for (j = 0; j < height; j++)
-	{
-	rw[i][j] = 0;
 	}
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+	for (wid_index = 0; wid_index < width; wid_index++)
+	twoD[hgt_index][wid_index] = 0;
 	}
-	return (rw);
+	return (twoD);
 }
