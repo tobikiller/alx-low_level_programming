de194c222064b9895e7fa0bd197e19e67b8e97b0
tobikiller <edwardnguimeya@yahoo.com>
2022-10-06 09:53:43 -0700

    malloc hecked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: size of memory to be allocated
+* Return: pointer to the allocated memory (SUCCESS)
+* 98 if insufficient memory was available (FAILURE)
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
