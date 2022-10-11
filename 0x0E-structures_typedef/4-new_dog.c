9c074d606d68bba6b085657bacb9353544681cb1
tobikiller <edwardnguimeya@yahoo.com>
2022-10-11 12:12:22 -0700

    compile done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,6 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < nlen; i++)
 		doggy->name[i] = name[i];
 	doggy->age = age;
+
 	doggy->owner = malloc(olen * sizeof(doggy->owner));
 	if (doggy == NULL)
 		return (NULL);
