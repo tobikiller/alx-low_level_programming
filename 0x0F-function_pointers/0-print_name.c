81d267664e79c350865fa956cf11f0a9f69c1824
tobikiller <edwardnguimeya@yahoo.com>
2022-10-12 11:00:55 -0700

    print my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: functiion pointer
  * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-    if(f)
-        f(name);
+	if (f)
+		f(name);
 }
