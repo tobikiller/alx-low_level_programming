e6318dcbe609e4d177911bbb01912e98ca542d68
Tobikiller <edwardnguimeya@yahoo.com>
2022-10-27 16:37:42 -0700

    task 0&1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,40 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+
+ * binary_to_uint - Converters a binary number to an unsigned integer
+ * @b: String representing a binary number
+ * Return: The converted number or 0 if b is NULL or if there are chars
+ * that are not 0 or 1
  */
+
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+	unsigned int sum;
+	unsigned int pow;
+	int base;
 
-	int len = 0, i;
-	unsigned int sum = 0;
-
+	base = 2;
+	sum = 0;
+	pow = 1;
 	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+		return (0);
+	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
+		return ((b[0] - 48));
+	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
+			pow = pow * base;
+		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
 		len--;
+		pow = 1; /*Reseting the exponent*/
 	}
-
 	return (sum);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+
+void print_b(unsigned long int n);
 /**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
+ * print_binary - Prints the binary representation of a number
+ * @n: Number to interpret as binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int n_copy = n;
-	int len = 0;
+	if (n == 0)
+		_putchar('0');
+	else
+		print_b(n);
+}
 
-	while (n_copy > 0)
-	{
-		len++;
-		n_copy >>= 1;
-	}
-	len -= 1;
-
-	while (len >= 0)
-	{
-		if ((n >> len) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		len--;
-	}
+/**
+ * print_b - Prints the binary representation of a number
+ * @n: Number to interpret as binary
+ */
+void print_b(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
 }
